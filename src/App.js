b09c638e63cf248ddce7d8d31396e05cf9562cb4
Cispas_M <crispasmakanani@gmail.com>
2023-01-09 09:25:48 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Movies from './componets/movies';
+import { Route } from 'react-router-dom'; 
 
 
 function App() {
   return (
     <main className='container'>
       <Movies />
+      <div className='content'>
+        <Route path="/products" components={Products} />
+        <Route path="/posts" components={Posts} />
+        <Route path="/admin" components={Dashboard} />
+        <Route path="/" components={Home} />
+      </div>
     </main>
   );
 }
